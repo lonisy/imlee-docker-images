97de67d39588ead994d989dcca7dab774983fcc5
I'm Lee <itffind@gmail.com>
2018-10-27 18:18:02 +0800

    提交项目文件

--- library/code-deploy/1.0/build.sh
+++ library/code-deploy/1.0/build.sh
@@ -1,6 +1,6 @@
 apk update
 apk upgrade
-apk add php-pear autoconf build-base linux-headers libaio-dev php7-dev libevent-dev rsync zip unzip wget
+apk add bash php-pear autoconf build-base linux-headers libaio-dev php7-dev libevent-dev rsync zip unzip wget
 
 pecl install http://pecl.php.net/get/swoole-4.2.1.tgz && echo "extension=swoole.so" > /etc/php7/conf.d/20_swoole.ini
 
