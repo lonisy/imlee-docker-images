6e22c5da1c560312431ff41a734f73fd5cd4975a
I'm Lee <itffind@gmail.com>
2018-10-27 16:06:01 +0800

    提交项目文件

--- library/code-deploy/1.0/service.sh
+++ library/code-deploy/1.0/service.sh
@@ -18,17 +18,21 @@ $http->on("start", function($server) {
     echo "Swoole http server is started at http://127.0.0.1:4433\n";
 });
 $http->on("request", function($request, $response) {
-    $output = 'Hello!';
+    $output = 'Hello:'.PHP_EOL;
     $data   = $request->rawContent();
     if (!empty($data)) {
         $data = json_decode($data, true);
-        if (!empty($data['repository']['name'])) {
+        if (isset($data['repository']['name'])) {
             $cmd = "/data/code-deploy/update-{$data['repository']['name']}.sh";
             if (file_exists($cmd) == true) {
                 $output = shell_exec($cmd);
             }else{
                 $output = $cmd.' not found!';
             }
+        }else{
+            $output .= 'GET:'.var_export($request->get).PHP_EOL;
+            $output .= 'POST:'.var_export($request->post).PHP_EOL;
+            $output .= 'REQUEST:'.var_export($request->getData()).PHP_EOL;
         }
     }
     $response->header("Content-Type", "text/plain");
