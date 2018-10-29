d37b8e3a8ca90bf63c7ec4b32e5588f80604d3dd
I'm Lee <itffind@gmail.com>
2018-10-29 15:02:26 +0800

    提交项目文件

--- library/code-deploy/1.0/build.sh
+++ library/code-deploy/1.0/build.sh
@@ -1,6 +1,8 @@
 apk update
 apk upgrade
 apk add bash openssh php-pear autoconf build-base linux-headers libaio-dev php7-dev libevent-dev rsync zip unzip wget
+apk add -U tzdata
+cp -r -f /usr/share/zoneinfo/Hongkong /etc/localtime
 
 pecl install http://pecl.php.net/get/swoole-4.2.1.tgz && echo "extension=swoole.so" > /etc/php7/conf.d/20_swoole.ini
 
