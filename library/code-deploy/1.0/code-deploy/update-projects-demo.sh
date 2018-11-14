e9079dbd5c4c9e1e4cdd5fa8e59cc4ca1d4c292f
I'm Lee <itffind@gmail.com>
2018-11-14 12:10:29 +0800

    提交项目文件

--- library/code-deploy/1.0/code-deploy/update-projects-demo.sh
+++ library/code-deploy/1.0/code-deploy/update-projects-demo.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 pushd /data/code-deploy/projects-demo && git pull && popd
-rsync -ap --delete --timeout=30 /data/code-deploy/projects-demo/* /data/projects-demo
+rsync -ap /data/code-deploy/projects-demo/* /data/projects-demo
+
