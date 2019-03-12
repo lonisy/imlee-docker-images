351124f757f2188fa2e63fa2735a7a179a41053c
I'm Lee <itffind@gmail.com>
2019-03-12 17:43:49 +0800

    no message

--- library/php/7.0.30-fpm/build.sh
+++ library/php/7.0.30-fpm/build.sh
@@ -2,7 +2,7 @@
 apt-get update --assume-yes && apt-get install -y libmagickwand-dev libmagickcore-dev
 apt-get install -y libicu-dev libpq-dev
 apt-get install -y libevent-dev libmemcached-dev libmemcached
-apt-get install -y zip unzip git rsync librdkafka-dev
+apt-get install -y zip unzip git rsync
 
 /usr/local/bin/docker-php-ext-install intl mysqli pdo_mysql pdo_pgsql pgsql shmop opcache sockets
 /usr/local/bin/docker-php-ext-configure gd \
@@ -18,9 +18,11 @@ apt-get install -y zip unzip git rsync librdkafka-dev
 
 # 安装扩展
 pecl install https://pecl.php.net/get/redis-3.1.3.tgz && echo "extension=redis.so" > /usr/local/etc/php/conf.d/redis.ini
-pecl install http://pecl.php.net/get/rdkafka-3.0.4.tgz && echo "extension=rdkafka.so" > /usr/local/etc/php/conf.d/rdkafka.ini
 pecl install http://pecl.php.net/get/SeasLog-2.0.2.tgz && echo "extension=seaslog.so" > /usr/local/etc/php/conf.d/seaslog.ini
-pecl install rdkafka
+#pecl install http://pecl.php.net/get/xdebug-2.5.5.tgz && echo "zend_extension=xdebug.so" > /usr/local/etc/php/conf.d/xdebug.ini
+apt install librdkafka-dev
+pecl install http://pecl.php.net/get/rdkafka-3.0.4.tgz && echo "extension=rdkafka.so" > /usr/local/etc/php/conf.d/rdkafka.ini
+
 
 # 安装 php-composer
 /usr/local/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && /usr/local/bin/php composer-setup.php --install-dir=/usr/bin --filename=composer && /usr/local/bin/php -r "unlink('composer-setup.php');"
