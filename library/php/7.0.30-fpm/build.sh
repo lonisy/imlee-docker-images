681e00d3e219b52495b6ebc66cbc1a20cefca1b4
I'm Lee <itffind@gmail.com>
2019-08-06 18:27:43 +0800

    no message

--- library/php/7.0.30-fpm/build.sh
+++ library/php/7.0.30-fpm/build.sh
@@ -33,7 +33,7 @@ pecl install https://pecl.php.net/get/swoole-2.2.0.tgz
 pecl install https://pecl.php.net/get/xdebug-2.5.5.tgz
 pecl install https://pecl.php.net/get/mongodb-1.5.3.tgz
 pecl install yaf
-/usr/local/bin/docker-php-ext-enable redis SeasLog rdkafka imagick swoole xdebug mongodb yaf
+/usr/local/bin/docker-php-ext-enable redis seaslog rdkafka imagick swoole xdebug mongodb yaf
 
 # 安装 php-composer
 /usr/local/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && /usr/local/bin/php composer-setup.php --install-dir=/usr/bin --filename=composer && /usr/local/bin/php -r "unlink('composer-setup.php');"
