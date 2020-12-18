32d68ed31f0324dee993014211e6fca78ed1cd7d
I'm Lee <itffind@gmail.com>
2020-12-18 12:04:51 +0800

    feat

--- library/php/7.2.8-fpm/build.sh
+++ library/php/7.2.8-fpm/build.sh
@@ -1,18 +1,4 @@
 #!/usr/bin/env bash
-apt-get update --assume-yes && apt-get install -y libmagickwand-dev libmagickcore-dev
-apt-get install -y libicu-dev libpq-dev
-apt-get install -y libevent-dev libmemcached-dev libmemcached
-apt-get install -y zip unzip git rsync
-
-/usr/local/bin/docker-php-ext-install intl mysqli pdo_mysql pdo_pgsql pgsql shmop opcache sockets
-/usr/local/bin/docker-php-ext-configure gd \
-        --with-gd \
-        --with-freetype-dir=/usr/include/ \
-        --with-png-dir=/usr/include/ \
-        --with-jpeg-dir=/usr/include/ \
-    && /usr/local/bin/docker-php-ext-install gd \
-    && /usr/local/bin/docker-php-ext-enable gd \
-    && /usr/local/bin/docker-php-ext-install calendar zip pcntl
 
 # 安装扩展
 pecl channel-update pecl.php.net
@@ -33,7 +19,5 @@ pecl clear-cache
 /usr/local/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && /usr/local/bin/php composer-setup.php --install-dir=/usr/bin --filename=composer && /usr/local/bin/php -r "unlink('composer-setup.php');"
 
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-
-apt-get autoclean && apt-get clean  && apt-get autoremove
 
 rm -rf /tmp/*
